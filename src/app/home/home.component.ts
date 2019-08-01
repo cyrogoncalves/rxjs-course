bbcbc8654ae7ac5a7a0e45d42a38c36754928625
Cyro Gonçalves <cyro.goncalves@footcion.club>
2019-08-01 19:27:31 -0300

    aula 17

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,24 +2,27 @@ import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {interval, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { createHttpObservable } from '../common/util';
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
 
+  ngOnInit() {
+    const http$ = createHttpObservable('/api/courses');
 
-    constructor() {
+    const courses$ = http$.pipe(
+      map(res => Object.values(res['payload']) as Course[]),
+      shareReplay()
+    );
 
-    }
+    this.beginnerCourses$ = courses$.pipe(map(courses => courses.filter(c => c.category == 'BEGINNER')));
+    this.advancedCourses$ = courses$.pipe(map(courses => courses.filter(c => c.category == 'ADVANCED')));
 
-    ngOnInit() {
-
-
-
-    }
-
+  }
 }
