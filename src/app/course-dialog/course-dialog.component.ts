7a60b7cd80b84b0a76343aac0312bd0004393825
Cyro Gonçalves <cyro.goncalves@footcion.club>
2019-08-05 18:06:49 -0300

    exhaustMap

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -57,7 +57,10 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    }
+      fromEvent(this.saveButton.nativeElement, 'click')
+        .pipe(exhaustMap(() => this.saveCourse(this.form.value)))
+        .subscribe()
+  }
 
     close() {
         this.dialogRef.close();
