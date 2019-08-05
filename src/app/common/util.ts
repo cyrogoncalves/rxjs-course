47f4eaa8dbdc33bbca2abf715b551b89f0a6184d
Cyro Gonçalves <cyro.goncalves@footcion.club>
2019-08-05 18:07:04 -0300

    AbortController signal

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -2,12 +2,17 @@ import { Observable } from 'rxjs';
 
 export function createHttpObservable(url : string) {
   return new Observable(observer => {
-    fetch(url)
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch(url, { signal })
       .then(response => response.json())
       .then(body => {
         observer.next(body);
         observer.complete();
       })
-      .catch(err => observer.error(err))
+      .catch(err => observer.error(err));
+
+    return () => controller.abort();
   });
 }
